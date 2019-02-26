93df9d8c4690ee00cce669c15ee9189aeb8af28b
Bao D <baocancode@gmail.com>
2019-02-26 21:26:16 +1100

    dev: create basic API server

---
+++ packages/api/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+export const start = async () => {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(4100)
+}
+
+if (require.main === module) {
+  start().catch(console.error)
+}
