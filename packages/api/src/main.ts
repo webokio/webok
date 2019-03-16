c829468d4659fe4699a9c6566be40f713446b812
Bao D <baocancode@gmail.com>
2019-03-17 10:20:07 +1100

    dev: update nestjs to v6

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -2,24 +2,25 @@ import fs from 'fs'
 import path from 'path'
 import { NestFactory } from '@nestjs/core'
 import { INestApplication } from '@nestjs/common'
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { configureFeatures } from './features'
 
-const configureDocs = async (app: INestApplication): Promise<void> => {
-  const { version } = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8'))
-  const options = new DocumentBuilder()
-    .setTitle('WebOK')
-    .setVersion(version)
-    .build()
-  const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('explorer', app, document)
-}
+// @nestjs/swagger does not work with nestjs v6 yet
+// const configureDocs = async (app: INestApplication): Promise<void> => {
+//   const { version } = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf-8'))
+//   const options = new DocumentBuilder()
+//     .setTitle('WebOK')
+//     .setVersion(version)
+//     .build()
+//   const document = SwaggerModule.createDocument(app, options)
+//   SwaggerModule.setup('explorer', app, document)
+// }
 
 export const start = async () => {
   const app: INestApplication = await NestFactory.create(AppModule)
   await configureFeatures(app)
-  await configureDocs(app)
+  // await configureDocs(app)
   await app.listen(process.env.PORT || 4100)
 }
 
