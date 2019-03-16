4dc8a896d301f2ef2bad8dd9b9d95657142e3a63
Bao D <baocancode@gmail.com>
2019-03-16 23:20:24 +1100

    dev: add vuetify to web

---
+++ packages/web/src/layouts/default.vue
@@ -0,0 +1,15 @@
+<script lang="tsx">
+import { Component, Vue } from 'vue-property-decorator'
+import { CreateElement } from 'vue'
+
+@Component({})
+export default class DefaultLayout extends Vue {
+  render (h: CreateElement) {
+    return (
+      <v-app>
+        <nuxt/>
+      </v-app>
+    )
+  }
+}
+</script>
