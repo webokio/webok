a53818aec06520d1f8b9f93f70b46d6e9c18c274
Bao D <baocancode@gmail.com>
2019-03-30 09:09:47 +1100

    dev: add common components

---
+++ packages/web/src/components/common/form-card.vue
@@ -0,0 +1,27 @@
+<script lang='tsx'>
+import { Component, Vue } from 'vue-property-decorator'
+import { CreateElement } from 'vue'
+
+@Component({})
+export default class FormCard extends Vue {
+  render (h: CreateElement) {
+    return (
+      <v-card raised>
+        <v-card-title
+          class='my-2 pb-0'
+          primary-title
+        >
+          {this.$slots.title}
+        </v-card-title>
+        <v-card-text class='py-0'>
+          {this.$slots.default}
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer/>
+          {this.$slots.actions}
+        </v-card-actions>
+      </v-card>
+    )
+  }
+}
+</script>
