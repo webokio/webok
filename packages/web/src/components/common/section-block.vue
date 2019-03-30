fa5506faacd407a9c714eaefcdf9be37f24235bf
Bao D <30614076+baocancode@users.noreply.github.com>
2019-03-30 21:44:50 +1100

    dev: build shared code to es6 (#45)
    - Build the shared code (core, client, helpers) to es6
    - Add some common components and allow to mock dependencies (such as @nestjs/swagger) in web

---
+++ packages/web/src/components/common/section-block.vue
@@ -0,0 +1,19 @@
+<script lang='tsx'>
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import { CreateElement } from 'vue'
+
+@Component({})
+export default class SectionBlock extends Vue {
+  @Prop({ type: String, required: true })
+  private readonly title!: string
+
+  render (h: CreateElement) {
+    return (
+      <div class='my-2'>
+        <strong>{this.title}</strong>
+        {this.$slots.default}
+      </div>
+    )
+  }
+}
+</script>
