10cf078c3196cababd046982ff50362422f4ebd4
Bao D <baocancode@gmail.com>
2019-04-12 23:08:52 +1000

    dev: update api urls

--- packages/client/src/authenticators/authenticator.helper.ts
+++ packages/client/src/authenticators/authenticator.helper.ts
@@ -1,4 +1,5 @@
-import { AuthDto } from '@webok/core/lib/auth'
+import { AuthDto, ModifyAuthDto } from '@webok/core/lib/auth'
+import { AxiosInstance } from 'axios'
 import jwtDecode from 'jwt-decode'
 
 export class AuthenticatorHelper {
@@ -6,5 +7,11 @@ export class AuthenticatorHelper {
     // Check if access token is already expired or will be expired soon (less than 1000 seconds)
     const { exp } = jwtDecode(auth.accessToken)
     return exp * 1000 - Date.now() < 1000
+  }
+
+  async refreshAuth (axios: AxiosInstance, auth: AuthDto): Promise<AuthDto> {
+    const { authId, refreshToken } = auth
+    const refreshData: ModifyAuthDto = { refreshToken }
+    return (await axios.post<AuthDto>(`/auth/${authId}/refresh`, refreshData)).data
   }
 }
