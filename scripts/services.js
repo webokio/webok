50584b7e649790c6e667697a8dd7da572b99da7a
Bao D <30614076+baocancode@users.noreply.github.com>
2019-04-17 22:50:59 +1000

    dev: move appId to script (#62)

--- scripts/services.js
+++ scripts/services.js
@@ -1,7 +1,7 @@
 const config = require('config')
 const { createServices } = require('../tools/dev/lib/services')
 
-const services = createServices(config)
+const services = createServices('webok', config)
 
 if (require.main === module) {
   services(process.argv.slice(2))
